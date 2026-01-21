9407cb59b789eed6ad630f8590d6e42fbe24e305
Raphael Schuhmann <raphael221@outlook.de>
2026-01-21 13:44:36 +0100

    Fixed invalid enum switch labels

--- src/main/java/com/mockra/api/errorHandling/ErrorHandler.java
+++ src/main/java/com/mockra/api/errorHandling/ErrorHandler.java
@@ -3,10 +3,10 @@ package com.mockra.api.errorHandling;
 public class ErrorHandler {
     public static void displayMessage(String msg, ErrorType type) {
         switch (type) {
-            case ErrorType.INFO -> System.out.println("\u001B[34m" + "[INFO] " + "\u001B[0m" + msg);
-            case ErrorType.WARNING -> System.out.println("\u001B[33m" + "[WARNING] " + msg + "\u001B[0m");
-            case ErrorType.ERROR -> System.err.println("\u001B[31m" + "[ERROR]" + msg + "\u001B[0m");
-            case ErrorType.FATAL -> {
+            case INFO -> System.out.println("\u001B[34m" + "[INFO] " + "\u001B[0m" + msg);
+            case WARNING -> System.out.println("\u001B[33m" + "[WARNING] " + msg + "\u001B[0m");
+            case ERROR -> System.err.println("\u001B[31m" + "[ERROR]" + msg + "\u001B[0m");
+            case FATAL -> {
                 System.err.println("\u001B[31m" + "[FATAL] " + msg + "\u001B[0m");
                 System.exit(1);
             }
