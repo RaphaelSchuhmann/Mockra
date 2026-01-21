365c701c1a9df8dc2fd5bb1e660f6e6cd1e9ba62
Raphael Schuhmann <raphael221@outlook.de>
2026-01-21 13:50:26 +0100

    Added handling for type being null and type being DEBUG

--- src/main/java/com/mockra/api/errorHandling/ErrorHandler.java
+++ src/main/java/com/mockra/api/errorHandling/ErrorHandler.java
@@ -2,6 +2,11 @@ package com.mockra.api.errorHandling;
 
 public class ErrorHandler {
     public static void displayMessage(String msg, ErrorType type) {
+        if (type == null) {
+            System.out.println("\u001B[34m" + "[INFO] " + "\u001B[0m" + msg);
+            return;
+        }
+
         switch (type) {
             case INFO -> System.out.println("\u001B[34m" + "[INFO] " + "\u001B[0m" + msg);
             case WARNING -> System.out.println("\u001B[33m" + "[WARNING] " + msg + "\u001B[0m");
@@ -10,6 +15,7 @@ public class ErrorHandler {
                 System.err.println("\u001B[31m" + "[FATAL] " + msg + "\u001B[0m");
                 System.exit(1);
             }
+            case DEBUG -> System.out.println("\u001B[35m" + "[INFO] " + "\u001B[0m" + msg);
             default -> System.out.println("[INFO] " + msg);
         }
     }
