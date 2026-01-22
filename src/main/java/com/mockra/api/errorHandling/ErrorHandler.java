b51b5ecb6806b303d08f41f9ba0fe3187ebc2d90
Raphael Schuhmann <raphael221@outlook.de>
2026-01-22 13:54:43 +0100

    Updated error handler to use lighter colors and simplified message construction

--- src/main/java/com/mockra/api/errorHandling/ErrorHandler.java
+++ src/main/java/com/mockra/api/errorHandling/ErrorHandler.java
@@ -8,14 +8,14 @@ public class ErrorHandler {
         }
 
         switch (type) {
-            case INFO -> System.out.println("\u001B[34m" + "[INFO] " + "\u001B[0m" + msg);
-            case WARNING -> System.out.println("\u001B[33m" + "[WARNING] " + msg + "\u001B[0m");
-            case ERROR -> System.err.println("\u001B[31m" + "[ERROR] " + msg + "\u001B[0m");
+            case INFO -> System.out.println("[\u001B[1m\u001B[34mINFO\u001B[0m] " + msg);
+            case WARNING -> System.out.println("[\u001B[1m\u001B[33mWARNING\u001B[0m] " + msg);
+            case ERROR -> System.err.println("[\u001B[1m\u001B[31mERROR\u001B[0m] " + msg);
             case FATAL -> {
-                System.err.println("\u001B[31m" + "[FATAL] " + msg + "\u001B[0m");
+                System.err.println("\u001B[1m\u001B[31m[FATAL] " + msg + "\u001B[0m");
                 System.exit(1);
             }
-            case DEBUG -> System.out.println("\u001B[35m" + "[DEBUG] " + "\u001B[0m" + msg);
+            case DEBUG -> System.out.println("\u001B[1m\u001B[35m" + "[DEBUG] " + "\u001B[0m" + msg);
             default -> System.out.println("[INFO] " + msg);
         }
     }
