76fb4e3e5b33cfab6e511a34622ecafff6e9675c
Raphael Schuhmann <raphael221@outlook.de>
2026-01-22 15:56:54 +0100

    Implemented ConfigController with an endpoint to hot reload the config

---
+++ src/main/java/com/mockra/api/config/ConfigController.java
@@ -0,0 +1,32 @@
+package com.mockra.api.config;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.mockra.api.errorHandling.ErrorHandler.displayMessage;
+
+import com.mockra.api.config.MockraConfig.EndpointConfig;
+import com.mockra.api.errorHandling.ErrorType;
+
+@RestController
+public class ConfigController {
+    private final ConfigService configService;
+
+    public ConfigController(ConfigService configService) {
+        this.configService = configService;
+    }
+
+    @GetMapping("/admin/config")
+    public String hotReloadConfig() {
+        try {
+            configService.load(false);
+            
+            displayMessage("Config reloaded successfully!", ErrorType.INFO);
+            return "Config reloaded successfully!";
+        } catch (Exception e) {
+            displayMessage(e.getMessage(), ErrorType.FATAL);
+            return "Config reload failed!";
+        }
+    }
+
+}
